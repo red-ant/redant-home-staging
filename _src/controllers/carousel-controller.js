59ac85001932a9c689ff2a96e1e8d789b6f088c0
Jason Taylor <jason@redant.com.au>
2022-04-28 16:01:46 +1000

    fix(carousel): update to stimulus-carousel

--- _src/controllers/carousel-controller.js
+++ _src/controllers/carousel-controller.js
@@ -1,125 +1,57 @@
 import { Controller } from "@hotwired/stimulus";
+import Carousel from "stimulus-carousel";
 import { random } from "../utils";
+import "swiper/css/bundle";
 
-export class CarouselController extends Controller {
-  static targets = ["carousel", "inner", "item"];
+export class CarouselController extends Carousel {
+  static targets = ["slide"];
 
   static values = {
-    activeClass: { type: String, default: "active" },
-    activateFirst: { type: Boolean, default: false },
-    isNormalised: { type: Boolean, default: false },
-    randomSlides: { type: Number, default: 0 },
-    slides: { type: Number, default: 0 },
+    random: { type: Boolean, default: false },
+    count: { type: Number, default: 0 },
   };
 
-  slides;
-
   connect() {
-    if (this.isNormalised) {
-      this.normalise();
+    if (this.randomValue) {
+      this.randomise();
     }
 
-    if (this.isMultiple) {
-      this.multiple();
+    if (this.slides.length > this.countValue) {
+      this.removeSlides();
     }
 
-    if (this.activateFirstValue) {
-      this.activateFirst();
-    }
+    super.connect();
   }
 
-  normalise() {
-    const height = this.height;
+  randomise() {
+    const slides = [...random(this.slides)];
 
-    this.items.forEach((el) => {
-      el.style.height = `${height}px`;
+    this.slides.forEach((item, i) => {
+      item.replaceWith(slides[i]);
     });
   }
 
-  activateFirst() {
-    this.items[0].classList.add(this.activeClass);
-    this.element.classList.remove("d-none");
-  }
-
-  multiple() {
-    const slides = this.items;
-
-    slides.forEach((el) => {
-      let next = el.nextElementSibling;
-
-      for (let i = 1; i < this.slidesValue; i++) {
-        if (!next) {
-          next = slides[0];
-        }
-
-        const cloneChild = next.cloneNode(true);
-        el.appendChild(cloneChild.children[0]);
-
-        next = next.nextElementSibling;
+  removeSlides() {
+    this.slides.forEach((item, i) => {
+      if (i >= this.countValue) {
+        item.remove();
       }
     });
   }
 
-  get isMultiple() {
-    return this.slidesValue > 1;
-  }
-
-  get isNormalised() {
-    return this.isNormalisedValue;
-  }
-
-  get height() {
-    return Math.max(...this.heights);
-  }
-
-  get heights() {
-    return this.items.map((el) => {
-      let height = el.offsetHeight;
-
-      if (!height) {
-        el.classList.toggle(this.activeClass);
-        height = el.offsetHeight;
-        el.classList.toggle(this.activeClass);
-      }
-
-      return height;
-    });
-  }
-
-  get carousel() {
-    return this.carouselTarget;
-  }
-
-  get inner() {
-    return this.innerTarget;
-  }
-
-  get items() {
-    if (this.slides) {
-      return this.slides;
+  get defaultOptions() {
+    return {
+      speed: 750,
+      spaceBetween: 35,
+      slidesPerView: 1.7,
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      loop: true,
+      autoplay: true,
     }
-
-    if (this.randomSlides > 0) {
-      this.slides = random(this.itemTargets).slice(0, this.randomSlides);
-
-      // remove unslected items
-      this.itemTargets.forEach((item) => {
-        if (!this.slides.includes(item)) {
-          item.remove();
-        }
-      });
-
-      return this.slides;
-    }
-
-    return this.itemTargets;
   }
 
-  get activeClass() {
-    return this.activeClassValue;
-  }
-
-  get randomSlides() {
-    return this.randomSlidesValue;
+  get slides() {
+    return this.slideTargets;
   }
 }
