e31e5d574e94b326315e7f8b01c92cb01855beda
Jason Taylor <jason@redant.com.au>
2022-05-02 15:51:57 +1000

    feat(on-demand): add the confirmation screens

--- _src/application.js
+++ _src/application.js
@@ -3,6 +3,6 @@ import Carousel from "bootstrap/js/dist/carousel.js";
 import Collapse from "bootstrap/js/dist/collapse.js";
 import Modal from "bootstrap/js/dist/modal.js";
 
-import "./aos";
+import "./lib/aos";
 import "./controllers";
 import "./stylesheets/application.scss";
