772e7220a0e4afd4df4224f8c8991af48119d622
Paulo Pao <paulo@redant.com.au>
2022-10-27 16:39:53 +1100

    feat(assessment): Fix navigation and add localstorage & question visibility logic

--- _src/controllers/assessment-controller.js
+++ _src/controllers/assessment-controller.js
@@ -1,23 +1,74 @@
 import { Controller } from "@hotwired/stimulus";
 
 export class AssessmentController extends Controller {
-  static targets = [ "section", "question", "content" ];
+  static targets = [ "section", "question", "answer", "resume" ];
 
   connect() {
+    this.checkIncomplete();
     this.setActiveSection();
     this.setActiveQuestion();
   }
 
   setActiveSection() {
-    // this.section.classList.add("active");
+    this.sectionTarget.classList.add("active");
   }
 
   setActiveQuestion() {
-    // this.questions.forEach((el) => el.classList.add("active"));
+    const params = this.getParams();
+    const question = params.has('q') ? parseInt(params.get('q')) - 1 : 0;
+
+    this.questionTargets[question].classList.remove("d-none");
+  }
+  
+  calculate() {}
+
+  checkIncomplete() {
+    const answers = this.getAnswers();
+
+    if (answers !== null) {
+      // this.resumeTarget.classList.add("show");
+    }
+  }
+  
+  clearAnswers() {
+    localStorage.removeItem("answers");
   }
 
-  select(question) {
-    // this.outputTarget.textContent =
-    //   `Hello, ${this.nameTarget.value}!`
+  getAnswers() {
+    const answers = localStorage.getItem("answers");
+    return answers === 'null' || answers === null ? {} : JSON.parse(answers);
+  }
+
+  getParams() {
+    return new Map(location.search.slice(1).split('&').map(kv => kv.split('=')));
+  }
+
+  resume() {
+    const answers = this.getAnswers();
+    // to do: find the last answer and 
+    // location.href = /assessment/section?q=answer;
+  }
+
+  select(event) {
+    this.setAnswer(event.params.answer);
+    location.href = event.target.href;
+  }
+
+  setAnswer(data) {
+    // data format: "section-slug,question-number,answer-number,answer-weight"
+    var answer = data.split(',');
+    var answers = this.getAnswers();
+
+    if (answers[answer[0]]) {
+      if (answers[answer[0]][answer[1]]) {
+        answers[answer[0]][answer[1]] = [answer[2], answer[3]];
+      } else {
+        answers[answer[0]].push([answer[2], answer[3]]);
+      }
+    } else {
+      answers[answer[0]] = [[answer[2], answer[3]]];
+    }
+
+    localStorage.setItem("answers", JSON.stringify(answers));
   }
 }
